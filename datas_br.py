fc6f547c342860f0a632806f6aafd8f403105bd7
Bruno <brox.fs@gmail.com>
2023-02-21 22:51:30 -0300

    Formatting and displaying date and time with datetime Lib.

--- datas_br.py
+++ datas_br.py
@@ -27,3 +27,7 @@ class DatasBr:
     def data_cadastro(self):
         data_formatada = self.momento_cadastro.strftime('%d/%m/%Y %H:%M')
         return data_formatada
+
+    def tempo_cadastro(self):
+        tempo_cadastro = datetime.today() - self.momento_cadastro
+        return tempo_cadastro
